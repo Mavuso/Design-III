ef63f64bbe6a1e771f8f2716b456891921c61660
Lindo <lindomcgud@gmail.com>
2018-10-02 17:35:59 +0200

    DB extraction module : functions :- monthly,daily,annually per  building.

--- python_tinyDB/db_extractor.py
+++ python_tinyDB/db_extractor.py
@@ -2,7 +2,7 @@
 Module for database extraction. This module will be used by modules on the logic layer
 @Author: L Magudulela
 '''
-from tinydb import TinyDB, where
+from tinydb import TinyDB, where, Query
 from tinydb import TinyDB
 from todate_fun import to_date
 import datetime, json
@@ -16,15 +16,30 @@ from serializer import DateTimeSerializer
 serialization = SerializationMiddleware()
 serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
 
+def get_dates(start =to_date("2013-01-01 00:00") ,end=to_date("2013-01-01 00:00"),collection = 'energy'):
+    query  = Query()
+    db = TinyDB('testdb.json',storage=serialization)
+    collection = db.table(collection)
+    return (collection.search((query.consumption['date']>= start) & (query.consumption['date']<= end)))
+
+#
+def get_yearly(year = '2013',collection = 'energy'
+    ,building = {"location":"Wits_The_Junction","name":"Shosholoza"}):
+    start = to_date(year + '-01-01 00:00')
+    end = to_date(year + '-01-01 00:00')
+    print(get_dates(start,end,'energy'))
 
 
+def get_monthly(year = 2013,month = '01',collection = 'energy'
+            ,building = {"location":"Wits_The_Junction","name":"Shosholoza"}):
+    start = to_date(str(year) +"-"+str(month)+'-01 00:00')
+    end = to_date(str(year) +"-"+str(month)+'-01 00:00')
+    print(get_dates(start,end,collection))
 
+def get_daily(year = 2013,month = '01',day=1,collection = 'energy'
+            ,building = {"location":"Wits_The_Junction","name":"Shosholoza"}):
+    start = to_date(str(year) +"-"+str(month)+'-'+str(day)+' 00:01')
+    end = to_date(str(year) +"-"+str(month)+'-'+str(day)+' 23:59')
+    print(get_dates(start,end,collection))
 
-
-#def get_year(date,collection = 'energy'):
-db = TinyDB('testdb.json',storage=serialization)
-collection = db.table('energy')
-print(collection.search(where("consumption")['date']== to_date('2013-01-01 17:00')))
-#print(collection.search(where("campus_name") == "Wits_The_Junction"))
-print("END")
-
+print(get_daily())
