08b48fe1e8866a53a10644c0ab972d8116bb7d24
Lindo <lindomcgud@gmail.com>
2018-10-09 17:23:45 +0200

    Change document IDs

--- NodeJS/create_uniqueID.py
+++ NodeJS/create_uniqueID.py
@@ -4,6 +4,6 @@ Fucntion creates unique IDs for each consumption document added.
 '''
 import re
 
-def create_docID(date,building_id):
-    id = re.sub(r'[^\w]', '', date) + building_id
+def create_docID(date,building_name,campus_name):
+    id = re.sub(r'[^\w]', '', date) + building_name[2:5] + campus_name[2:5] 
     return id
